92dc4660a222613c18712b39cb834bc3286241bb
Lopez <carlos.alfonzo@gmail.com>
2017-08-10 02:14:40 -0700

    added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Taco Burger');
+INSERT INTO burgers (burger_name) VALUES ('A1 Burger');
+INSERT INTO burgers (burger_name) VALUES ('Animal Burger');
+INSERT INTO burgers (burger_name) VALUES ('Flintstones Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicago Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', true);
