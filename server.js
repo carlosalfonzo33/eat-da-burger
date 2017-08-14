c64cf45e821f1cb9244102272c63904451cdee84
Lopez <carlos.alfonzo@gmail.com>
2017-08-14 14:51:27 -0700

    added heroku functionality

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
 
 // Open Server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port);
